7f07223d00dd9ac6ef140b6096f9bd6573415df1
240787 Ruszer Julia <240787@edu.p.lodz.pl>
2024-12-08 15:24:02 +0100

    change in RequestMapping

--- backendIo/src/main/java/org/ioad/spring/request/controllers/RequestController.java
+++ backendIo/src/main/java/org/ioad/spring/request/controllers/RequestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/request")
 public class RequestController {
 
     @Autowired
