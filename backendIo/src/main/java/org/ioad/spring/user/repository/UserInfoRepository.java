12a16d4921d8545baeee4c27149cc1c2e6e82ef3
CountToFour <247806@edu.p.lodz.pl>
2025-01-13 15:25:16 +0100

    connect tables user and userInfo after register

--- backendIo/src/main/java/org/ioad/spring/user/repository/UserInfoRepository.java
+++ backendIo/src/main/java/org/ioad/spring/user/repository/UserInfoRepository.java
@@ -3,8 +3,6 @@ package org.ioad.spring.user.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.ioad.spring.security.postgresql.models.ERole;
-import org.ioad.spring.user.models.EUserRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ioad.spring.security.postgresql.models.User;
 import org.springframework.stereotype.Repository;
@@ -14,5 +12,4 @@ import org.ioad.spring.user.models.UserInfo;
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     Optional<UserInfo> findByUser(User user);
-    List<UserInfo> findByRole(EUserRoles role);
 }
