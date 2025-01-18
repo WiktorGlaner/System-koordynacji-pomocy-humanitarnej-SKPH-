e7788c01b911dc290153e67dc956fbdc2c2c580f
Dawid Frontczak <dawid36980@wp.pl>
2025-01-18 15:42:20 +0100

    commit

---
+++ backendIo/src/main/java/org/ioad/spring/reports/Report.java
@@ -0,0 +1,223 @@
+package org.ioad.spring.reports;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.ioad.spring.request.models.Request;
+import org.ioad.spring.request.services.IRequestService;
+import org.ioad.spring.resource.models.Donation;
+import org.ioad.spring.resource.models.Resource;
+import org.ioad.spring.resource.models.ResourceType;
+import org.ioad.spring.resource.services.ResourceService;
+import org.ioad.spring.security.postgresql.models.User;
+import org.ioad.spring.task.model.Task;
+import org.ioad.spring.user.models.UserInfo;
+import org.ioad.spring.user.repository.UserInfoRepository;
+
+
+import org.ioad.spring.task.repository.TaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Date;
+@Service
+public class Report {
+    @Autowired
+    private TaskRepo taskRepo;
+    @Autowired
+    private IRequestService requestService;
+    @Autowired
+    private ResourceService resourceService;
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    Date nowDate = new Date();
+//    JasperPrint createTaxReport(long userId) throws JRException {
+//      System.out.println(resourceService.getSumOfQuantityByTypeAndDonorId(ResourceType.FINANCIAL, userId));
+//
+//        Map<String, Object> parameters = new HashMap<>();
+//        double kwotaa = resourceService.getSumOfQuantityByTypeAndDonorId(ResourceType.FINANCIAL, userId);
+//        parameters.put("kwota", kwotaa);
+//        parameters.put("Data", nowDate);
+//        String filePath = "C:\\Users\\dawid\\Desktop\\studia-3rok\\io\\projekt2\\System-koordynacji-pomocy-humanitarnej-SKPH--master\\backendIo\\src\\main\\java\\org\\ioad\\spring\\reports\\TaxReport.jrxml";
+//        JasperReport report = JasperCompileManager.compileReport(filePath);
+//        JasperPrint print = JasperFillManager.fillReport(report, parameters);
+//        return print;
+//    }
+
+//   public JasperPrint createReceiptReport(long userId) throws JRException {
+//        List<Donation> donations = new ArrayList<>();
+//        List<Donation> donations2 = new ArrayList<>();
+//        donations2.addAll(resourceService.getByDonationDonorId(userId));
+//        donations.add(donations2.get(0));
+//        donations.addAll(resourceService.getByDonationDonorId(userId));
+//       // donations.addAll(resourceService.getAllDonations());
+//        JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(donations);
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("resourcesDataSet3", taskDataSource);
+//        parameters.put("Data", nowDate);
+//       String filePath = "DonationReport.jrxml";
+//       InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+//
+//       if (reportStream == null) {
+//           throw new IllegalArgumentException("Plik raportu nie został znaleziony: " + filePath);
+//       }
+//       JasperReport report = JasperCompileManager.compileReport(filePath);
+//       JasperPrint print = JasperFillManager.fillReport(report, parameters, taskDataSource);
+//        return print;
+//
+//    }
+public JasperPrint createReceiptReport(long userId) throws JRException {
+    // Pobieranie danych
+        List<Donation> donations = new ArrayList<>();
+        List<Donation> donations2 = new ArrayList<>();
+        donations2.addAll(resourceService.getByDonationDonorId(userId));
+        donations.add(donations2.get(0));
+        donations.addAll(resourceService.getByDonationDonorId(userId));
+
+    // Utworzenie źródła danych dla raportu
+    JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(donations);
+
+    // Przygotowanie parametrów
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("resourcesDataSet3", taskDataSource);
+    parameters.put("Data", nowDate);
+
+    // Wczytanie pliku raportu jako zasobu
+    String filePath = "DonationReport.jrxml";
+    InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+
+    if (reportStream == null) {
+        throw new IllegalArgumentException("Plik raportu nie został znaleziony: " + filePath);
+    }
+
+    // Kompilacja i wypełnienie raportu
+    JasperReport report = JasperCompileManager.compileReport(reportStream);
+    JasperPrint print = JasperFillManager.fillReport(report, parameters, taskDataSource);
+    return print;
+}
+
+
+//    public JasperPrint createResourcesReport() throws JRException {
+//        List<Resource> resources = new ArrayList<>();
+//        resources.add(resourceService.getResourceById(1111));
+//        resources.addAll(resourceService.getAllResources());
+//        JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(resources);
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("resourcesDataSet3", taskDataSource);
+//        parameters.put("Data", nowDate);
+//        String filePath = "C:\\Users\\dawid\\Desktop\\studia-3rok\\io\\projekt2\\System-koordynacji-pomocy-humanitarnej-SKPH--master\\backendIo\\src\\main\\java\\org\\ioad\\spring\\reports\\ResourceReport.jrxml";
+//        JasperReport report = JasperCompileManager.compileReport(filePath);
+//        JasperPrint print = JasperFillManager.fillReport(report, parameters, taskDataSource);
+//        return print;
+//
+//    }
+public JasperPrint createResourcesReport() throws JRException {
+    List<Resource> resources = new ArrayList<>();
+    resources.add(resourceService.getResourceById(1111));
+    resources.addAll(resourceService.getAllResources());
+
+    JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(resources);
+
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("resourcesDataSet3", taskDataSource);
+    parameters.put("Data", nowDate);
+
+    String filePath = "ResourceReport.jrxml";
+    InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+
+    if (reportStream == null) {
+        throw new IllegalArgumentException("Plik raportu nie został znaleziony: " + filePath);
+    }
+
+    JasperReport report = JasperCompileManager.compileReport(reportStream);
+    return JasperFillManager.fillReport(report, parameters, taskDataSource);
+}
+
+//    public JasperPrint createRequestReport() throws JRException {
+//        List<Request> requests = new ArrayList<>();
+//        requests.add(requestService.getRequestById(1L));
+//        requests.addAll(requestService.getAllRequests());
+//        JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(requests);
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("resourcesDataSet2", taskDataSource);
+//        parameters.put("Data", nowDate);
+//        String filePath = "C:\\Users\\dawid\\Desktop\\studia-3rok\\io\\projekt2\\System-koordynacji-pomocy-humanitarnej-SKPH--master\\backendIo\\src\\main\\java\\org\\ioad\\spring\\reports\\RequestReport.jrxml";
+//        JasperReport report = JasperCompileManager.compileReport(filePath);
+//        JasperPrint print = JasperFillManager.fillReport(report, parameters, taskDataSource);
+//        return print;
+//
+//    }
+public JasperPrint createRequestReport() throws JRException {
+    List<Request> requests = new ArrayList<>();
+    requests.add(requestService.getRequestById(1L));
+    requests.addAll(requestService.getAllRequests());
+
+    JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(requests);
+
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("resourcesDataSet2", taskDataSource);
+    parameters.put("Data", nowDate);
+
+    String filePath = "RequestReport.jrxml";
+    InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+
+    if (reportStream == null) {
+        throw new IllegalArgumentException("Plik raportu nie został znaleziony: " + filePath);
+    }
+
+    JasperReport report = JasperCompileManager.compileReport(reportStream);
+    return JasperFillManager.fillReport(report, parameters, taskDataSource);
+}
+
+//    public JasperPrint createHelpActivitiesReport() throws JRException {
+//        List<Task> tasks = new ArrayList<>();
+//        List<Task> tasks2 = new ArrayList<>();
+//        tasks2.addAll(taskRepo.findAll());
+//        tasks.add(tasks2.get(0));
+//        tasks.addAll(taskRepo.findAll());
+//        JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(tasks);
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("resourcesDataSet", taskDataSource);
+//        parameters.put("Data", nowDate);
+//
+//        String filePath = "C:\\Users\\dawid\\Desktop\\studia-3rok\\io\\projekt2\\System-koordynacji-pomocy-humanitarnej-SKPH--master\\backendIo\\src\\main\\java\\org\\ioad\\spring\\reports\\HelpActivitiesReport.jrxml";
+//        JasperReport report = JasperCompileManager.compileReport(filePath);
+//        JasperPrint print = JasperFillManager.fillReport(report, parameters, taskDataSource);
+//        return print;
+//
+//
+//    }
+public JasperPrint createHelpActivitiesReport() throws JRException {
+        List<Task> tasks = new ArrayList<>();
+        List<Task> tasks2 = new ArrayList<>();
+        tasks2.addAll(taskRepo.findAll());
+        tasks.add(tasks2.get(0));
+        tasks.addAll(taskRepo.findAll());
+
+    JRBeanCollectionDataSource taskDataSource = new JRBeanCollectionDataSource(tasks);
+
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("resourcesDataSet", taskDataSource);
+    parameters.put("Data", nowDate);
+
+    String filePath = "HelpActivitiesReport.jrxml";
+    InputStream reportStream = getClass().getClassLoader().getResourceAsStream(filePath);
+
+    if (reportStream == null) {
+        throw new IllegalArgumentException("Plik raportu nie został znaleziony: " + filePath);
+    }
+
+    JasperReport report = JasperCompileManager.compileReport(reportStream);
+    return JasperFillManager.fillReport(report, parameters, taskDataSource);
+}
+//
+//    JasperPrint createHelpProvidedReport() {
+//
+//    }
+}
---
+++ ioFrontend/frontendio/src/components/Report.vue
@@ -0,0 +1,475 @@
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>Generowanie Raportów</h1>-->
+
+<!--    &lt;!&ndash; Sprawdzamy, czy użytkownik ma odpowiednią rolę &ndash;&gt;-->
+<!--    <div v-if="allowedRole">-->
+<!--      <div>-->
+<!--        <label for="report-type">Wybierz typ raportu:</label>-->
+<!--        <select v-model="selectedOption" id="report-type">-->
+<!--          <option v-for="option in options" :key="option.value" :value="option.value">-->
+<!--            {{ option.label }}-->
+<!--          </option>-->
+<!--        </select>-->
+<!--      </div>-->
+
+<!--      <button @click="acceptOption">Zatwierdź raport</button>-->
+
+<!--      &lt;!&ndash; Możemy tutaj dodać iframe lub inną metodę do wyświetlania wygenerowanego raportu &ndash;&gt;-->
+
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--      <p>Nie masz uprawnień do generowania raportów.</p>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+
+<!--<script>-->
+<!--import ReportService from "@/services/report.service.js";-->
+
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      options: [-->
+<!--        { value: 'tasks', label: 'Raport dotyczący zadań' },-->
+<!--        { value: 'applications', label: 'Raport dotyczący wniosków o pomoc' },-->
+<!--        { value: 'resources', label: 'Raport dotyczący zasobów' },-->
+<!--      ],-->
+<!--      selectedOption: null,-->
+<!--      reportUrl: null,  // Zmienna do przechowywania linku do wygenerowanego raportu-->
+<!--      allowedRoles: ["ROLE_ORGANIZATION", "ROLE_DONOR", "ROLE_AUTHORITY"],  // Role, które mają dostęp do raportów-->
+<!--    };-->
+<!--  },-->
+<!--  computed: {-->
+<!--    currentUser() {-->
+<!--      return this.$store.state.auth.user;  // Pobieramy aktualnego użytkownika z Vuex-->
+<!--    },-->
+<!--    allowedRole() {-->
+<!--      // Sprawdzamy, czy użytkownik ma odpowiednią rolę-->
+<!--      return this.currentUser && this.allowedRoles.some(role => this.currentUser.roles.includes(role));-->
+<!--    },-->
+<!--  },-->
+<!--  methods: {-->
+<!--    // Funkcja, która jest wywoływana po kliknięciu przycisku "Zatwierdź raport"-->
+<!--    async acceptOption() {-->
+<!--      if (!this.selectedOption) {-->
+<!--        alert('Proszę wybrać typ raportu.');-->
+<!--        return;-->
+<!--      }-->
+
+<!--      if (!this.allowedRole) {-->
+<!--        alert('Nie masz uprawnień do generowania raportu.');-->
+<!--        return;-->
+<!--      }-->
+
+<!--      try {-->
+<!--        // Wywołanie metody do generowania raportu z wybranym typem-->
+<!--        await this.generateReport(this.selectedOption);-->
+<!--      } catch (error) {-->
+<!--        console.error('Błąd podczas generowania raportu:', error);-->
+<!--        alert('Nie udało się wygenerować raportu.');-->
+<!--      }-->
+<!--    },-->
+
+<!--    // Funkcja do generowania raportu-->
+<!--    async generateReport(reportType) {-->
+<!--      try {-->
+<!--        await ReportService.generateReport(reportType);  // Przesyłanie typu raportu-->
+<!--      } catch (error) {-->
+<!--        console.error('Błąd podczas generowania raportu:', error);-->
+<!--        alert('Błąd generowania raportu.');-->
+<!--      }-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--label {-->
+<!--  margin-right: 10px;-->
+<!--}-->
+<!--button {-->
+<!--  margin-left: 10px;-->
+<!--}-->
+<!--</style>-->
+<!--<template>-->
+<!--  <div class="report-container">-->
+<!--    <h1>Generowanie Raportów</h1>-->
+
+<!--    &lt;!&ndash; Sprawdzamy, czy użytkownik ma odpowiednią rolę &ndash;&gt;-->
+<!--    <div v-if="allowedRole">-->
+<!--      <div class="report-selection">-->
+<!--        <label for="report-type">Wybierz typ raportu:</label>-->
+<!--        <select v-model="selectedOption" id="report-type">-->
+<!--          <option v-for="option in allowedRoles" :key="option.value" :value="option.value">-->
+<!--            {{ option.label }}-->
+<!--          </option>-->
+<!--        </select>-->
+<!--        <button @click="downloadReport">-->
+<!--          <i class="fa fa-download"></i> Pobierz raport PDF-->
+<!--        </button>-->
+<!--        <button @click="showReportPreview">-->
+<!--          <i class="fa fa-eye"></i> Podgląd raportu-->
+<!--        </button>-->
+<!--      </div>-->
+
+<!--      &lt;!&ndash; Podgląd raportu w iframe &ndash;&gt;-->
+<!--      <div class="report-preview" v-if="reportUrl">-->
+<!--        <h2>Podgląd raportu:</h2>-->
+<!--        <iframe :src="reportUrl" frameborder="0"></iframe>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--      <p>Nie masz uprawnień do generowania raportów.</p>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import ReportService from "@/services/report.service.js";-->
+
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      options: [-->
+<!--        {userRole: ["ROLE_DONOR", "ROLE_AUTHORITY"] ,  value: "tasks", label: "Raport dotyczący zadań"},-->
+<!--        {userRole: ["ROLE_DONOR", "ROLE_AUTHORITY"], value: "applications", label: "Raport dotyczący wniosków o pomoc" },-->
+<!--        {userRole: ["ROLE_DONOR", "ROLE_AUTHORITY"],  value: "resources", label: "Raport dotyczący zasobów" },-->
+<!--      ],-->
+<!--      selectedOption: null,-->
+<!--      reportUrl: null, // URL dla iframe-->
+<!--      userRoles: ["ROLE_ORGANIZATION", "ROLE_DONOR", "ROLE_AUTHORITY"], // Role z dostępem do raportów-->
+
+<!--    };-->
+<!--  },-->
+
+<!--  computed: {-->
+<!--    filteredOptions() {-->
+<!--      // Filtruj opcje na podstawie roli użytkownika-->
+<!--      console.log(this.currentUser)-->
+<!--      return this.options.filter(option => option.userRole.includes(this.currentUser.roles[0]));-->
+<!--      //return this.currentUser && this.userRole.some((role) => this.currentUser.roles.includes(role));-->
+<!--    },-->
+
+<!--    currentUser() {-->
+<!--      return this.$store.state.auth.user; // Pobieramy aktualnego użytkownika z Vuex-->
+<!--    },-->
+<!--    allowedRole() {-->
+<!--      // Sprawdzamy, czy użytkownik ma odpowiednią rolę-->
+<!--      return this.currentUser && this.userRoles.some((role) => this.currentUser.roles.includes(role));-->
+
+<!--    },-->
+<!--  },-->
+<!--  methods: {-->
+<!--    // Pobieranie raportu PDF-->
+<!--    async downloadReport() {-->
+<!--      if (!this.selectedOption) {-->
+<!--        alert("Proszę wybrać typ raportu.");-->
+<!--        return;-->
+<!--      }-->
+<!--      try {-->
+<!--        await ReportService.generateReport(this.selectedOption); // Pobieranie raportu-->
+<!--      } catch (error) {-->
+<!--        console.error("Błąd podczas pobierania raportu:", error);-->
+<!--        alert("Nie udało się pobrać raportu.");-->
+<!--      }-->
+<!--    },-->
+
+<!--    // Podgląd raportu w iframe-->
+<!--    async showReportPreview() {-->
+<!--      if (!this.selectedOption) {-->
+<!--        alert("Proszę wybrać typ raportu.");-->
+<!--        return;-->
+<!--      }-->
+<!--      try {-->
+<!--        const url = await ReportService.previewReport(this.selectedOption);-->
+<!--        this.reportUrl = url; // Przechowywanie URL raportu-->
+<!--      } catch (error) {-->
+<!--        console.error("Błąd podczas podglądu raportu:", error);-->
+<!--        alert("Nie udało się wyświetlić raportu.");-->
+<!--      }-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+<!--<template>-->
+<!--  <div class="report-container">-->
+<!--    <h1>Generowanie Raportów</h1>-->
+
+<!--    &lt;!&ndash; Sprawdzamy, czy użytkownik ma odpowiednią rolę &ndash;&gt;-->
+<!--    <div v-if="allowedRole">-->
+<!--      <div class="report-selection">-->
+<!--        <label for="report-type">Wybierz typ raportu:</label>-->
+<!--        <select v-model="selectedOption" id="report-type">-->
+<!--          <option v-for="option in options" :key="option.value" :value="option.value">-->
+<!--            {{ option.label }}-->
+<!--          </option>-->
+<!--        </select>-->
+<!--        <button @click="downloadReport">-->
+<!--          <i class="fa fa-download"></i> Pobierz raport PDF-->
+<!--        </button>-->
+<!--        <button @click="showReportPreview">-->
+<!--          <i class="fa fa-eye"></i> Podgląd raportu-->
+<!--        </button>-->
+<!--      </div>-->
+
+<!--      &lt;!&ndash; Podgląd raportu w iframe &ndash;&gt;-->
+<!--      <div class="report-preview" v-if="reportUrl">-->
+<!--        <h2>Podgląd raportu:</h2>-->
+<!--        <iframe :src="reportUrl" frameborder="0"></iframe>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--      <p>Nie masz uprawnień do generowania raportów.</p>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import ReportService from "@/services/report.service.js";-->
+
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      options: [-->
+<!--        { value: "tasks", label: "Raport dotyczący zadań" },-->
+<!--        { value: "applications", label: "Raport dotyczący wniosków o pomoc" },-->
+<!--        { value: "resources", label: "Raport dotyczący zasobów" },-->
+<!--      ],-->
+<!--      selectedOption: null,-->
+<!--      reportUrl: null, // URL dla iframe-->
+<!--      allowedRoles: ["ROLE_ORGANIZATION", "ROLE_DONOR", "ROLE_AUTHORITY"], // Role z dostępem do raportów-->
+<!--    };-->
+<!--  },-->
+<!--  computed: {-->
+<!--    currentUser() {-->
+<!--      return this.$store.state.auth.user; // Pobieramy aktualnego użytkownika z Vuex-->
+<!--    },-->
+<!--    allowedRole() {-->
+<!--      // Sprawdzamy, czy użytkownik ma odpowiednią rolę-->
+<!--      return this.currentUser && this.allowedRoles.some((role) => this.currentUser.roles.includes(role));-->
+<!--    },-->
+<!--  },-->
+<!--  methods: {-->
+<!--    // Pobieranie raportu PDF-->
+<!--    async downloadReport() {-->
+<!--      if (!this.selectedOption) {-->
+<!--        alert("Proszę wybrać typ raportu.");-->
+<!--        return;-->
+<!--      }-->
+<!--      try {-->
+<!--        await ReportService.generateReport(this.selectedOption); // Pobieranie raportu-->
+<!--      } catch (error) {-->
+<!--        console.error("Błąd podczas pobierania raportu:", error);-->
+<!--        alert("Nie udało się pobrać raportu.");-->
+<!--      }-->
+<!--    },-->
+
+<!--    // Podgląd raportu w iframe-->
+<!--    async showReportPreview() {-->
+<!--      if (!this.selectedOption) {-->
+<!--        alert("Proszę wybrać typ raportu.");-->
+<!--        return;-->
+<!--      }-->
+<!--      try {-->
+<!--        const url = await ReportService.previewReport(this.selectedOption);-->
+<!--        this.reportUrl = url; // Przechowywanie URL raportu-->
+<!--      } catch (error) {-->
+<!--        console.error("Błąd podczas podglądu raportu:", error);-->
+<!--        alert("Nie udało się wyświetlić raportu.");-->
+<!--      }-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+
+<template>
+  <div class="report-container">
+    <h1> {{ $t('report-title') }}</h1>
+
+    <!-- Sprawdzamy, czy użytkownik ma odpowiednią rolę -->
+    <div v-if="allowedRole">
+      <div class="report-selection">
+        <label for="report-type">{{ $t('report-type') }}</label>
+        <select v-model="selectedOption" id="report-type">
+          <option v-for="option in options" :key="option.value" :value="option.value">
+            {{ option.label }}
+          </option>
+        </select>
+        <button @click="downloadReport">
+          <i class="fa fa-download"></i> {{ $t('report-download') }}
+        </button>
+        <button @click="showReportPreview">
+          <i class="fa fa-eye"></i> {{ $t('report-view') }}
+        </button>
+      </div>
+
+      <!-- Podgląd raportu w iframe -->
+      <div class="report-preview" v-if="reportUrl">
+        <h2>Podgląd raportu:</h2>
+        <iframe :src="reportUrl" frameborder="0"></iframe>
+      </div>
+    </div>
+    <div v-else>
+      <p>Nie masz uprawnień do generowania raportów.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReportService from "@/services/report.service.js";
+
+export default {
+  data() {
+    return {
+      options: [
+        { value: "tasks", label: "Raport dotyczący zadań" },
+        { value: "applications", label: "Raport dotyczący wniosków o pomoc" },
+        { value: "resources", label: "Raport dotyczący zasobów" },
+        { value: "donations", label: "Raport dotyczący dotacji" },
+        { value: "taxes", label: "Raport ulgi podatkowej" },
+      ],
+      selectedOption: null,
+      reportUrl: null, // URL dla iframe
+      allowedRoles: ["ROLE_ORGANIZATION", "ROLE_DONOR", "ROLE_AUTHORITY"], // Role z dostępem do raportów
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user; // Pobieramy aktualnego użytkownika z Vuex
+    },
+    allowedRole() {
+      // Sprawdzamy, czy użytkownik ma odpowiednią rolę
+      console.log(this.currentUser)
+
+      return this.currentUser && this.allowedRoles.some((role) => this.currentUser.roles.includes(role));
+    },
+  },
+  methods: {
+    // Pobieranie raportu PDF
+    async downloadReport() {
+      if (!this.selectedOption) {
+        alert("Proszę wybrać typ raportu.");
+        return;
+      }
+      try {
+        console.log(this.selectedOption)
+        await ReportService.generateReport(this.selectedOption, this.$store.state.auth.user.id); // Pobieranie raportu
+      } catch (error) {
+        console.error("Błąd podczas pobierania raportu:", error);
+        alert("Nie udało się pobrać raportu.");
+      }
+    },
+
+    // Podgląd raportu w iframe
+    async showReportPreview() {
+      if (!this.selectedOption) {
+        alert("Proszę wybrać typ raportu.");
+        return;
+      }
+      try {
+        const url = await ReportService.previewReport(this.selectedOption, this.$store.state.auth.user.id);
+        this.reportUrl = url; // Przechowywanie URL raportu
+      } catch (error) {
+        console.error("Błąd podczas podglądu raportu:", error);
+        alert("Nie udało się wyświetlić raportu.");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.report-container {
+  font-family: 'Roboto', sans-serif;
+  margin: 40px auto;
+  max-width: 900px;
+  padding: 30px;
+  background-color: #ffffff;
+  border-radius: 12px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.15);
+}
+
+
+h1 {
+  text-align: center;
+  color: #2c3e50;
+  font-size: 28px;
+  font-weight: 700;
+  margin-bottom: 20px;
+}
+
+
+.report-selection {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 30px;
+}
+
+label {
+  font-weight: 600;
+  color: #34495e;
+  font-size: 16px;
+}
+
+
+select {
+  padding: 12px 16px;
+  border: 1px solid #dcdcdc;
+  border-radius: 6px;
+  font-size: 16px;
+  color: #34495e;
+  background-color: #f9f9f9;
+  transition: border-color 0.3s ease;
+}
+
+select:focus {
+  border-color: #3498db;
+  outline: none;
+}
+
+button {
+  padding: 12px 20px;
+  background-color: #3498db;
+  color: white;
+  font-size: 16px;
+  font-weight: 600;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+
+button:disabled {
+  background-color: #bdc3c7;
+  cursor: not-allowed;
+}
+
+button:hover:not(:disabled) {
+  background-color: #2980b9;
+  transform: scale(1.02);
+}
+
+button:active:not(:disabled) {
+  background-color: #2471a3;
+  transform: scale(1);
+}
+
+
+.report-preview {
+  margin-top: 20px;
+  text-align: center;
+}
+
+iframe {
+  width: 100%;
+  height: 600px;
+  border: 1px solid #dcdcdc;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+</style>
