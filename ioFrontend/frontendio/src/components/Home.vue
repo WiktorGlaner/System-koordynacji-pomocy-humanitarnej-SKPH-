b2143f2b60752fe25a89c0bdf2dcb7bb893099bf
Wiktor Glaner <247664@edu.p.lodz.pl>
2025-01-19 19:59:18 +0100

    changes in displaying home tiles for different roles

--- ioFrontend/frontendio/src/components/Home.vue
+++ ioFrontend/frontendio/src/components/Home.vue
@@ -29,15 +29,16 @@ const content = ref("");
 const router = useRouter();
 
 const tiles = ref([
-  { id: 1, title: t('home-raports'), description: t('home-raports-description'), icon: "fas fa-file-alt", link: "/reports", roles: ["ROLE_AUTHORITY", "ROLE_DONOR"] },
+  { id: 1, title: t('home-raports'), description: t('home-raports-description'), icon: "fas fa-file-alt", link: "/reports", roles: ["ROLE_AUTHORITY", "ROLE_DONOR", "ROLE_ORGANIZATION"] },
   { id: 2, title: t('home-profile'), description: t('home-profile-description'), icon: "fas fa-user", link: "/profile", roles: ["ROLE_USER","ROLE_VICTIM", "ROLE_VOLUNTEER", "ROLE_AUTHORITY", "ROLE_ORGANIZATION","ROLE_DONOR"] },
   { id: 3, title: t('home-resource'), description: t('home-resource-description'), icon: "fas fa-cogs", link: "/resource", roles: ["ROLE_ORGANIZATION", "ROLE_AUTHORITY", "ROLE_DONOR"] },
   { id: 4, title: t('home-map'), description: t('home-map-description'), icon: "fas fa-map", link: "/map", roles: ["ROLE_AUTHORITY","ROLE_VICTIM","ROLE_ORGANIZATION"] },
   { id: 5, title: t('home-chat'), description: t('home-chat-description'), icon: "fas fa-comments", link: "/communication", roles: ["ROLE_AUTHORITY","ROLE_VICTIM","ROLE_ORGANIZATION","ROLE_VOLUNTEER","ROLE_DONOR"] },
   { id: 6, title: t('home-volunteer'), description: t('home-volunteer-description'), icon: "fas fa-map", link: "/volunteer", roles: ["ROLE_ORGANIZATION"] },
   { id: 7, title: t('home-application'), description: t('home-application-description'), icon: "fas fa-map", link: "/application", roles: ["ROLE_ORGANIZATION"] },
-  { id: 8, title: t('home-application'), description: t('home-organizationVol-description'), icon: "fas fa-map", link: "/organization", roles: ["ROLE_VOLUNTEER"] },
-  { id: 8, title: t('home-organization'), description: t('home-organization-description'), icon: "fas fa-map", link: "/organization", roles: ["ROLE_AUTHORITY"] },
+  { id: 8, title: t('home-organization'), description: t('home-organization-description'), icon: "fas fa-map", link: "/organization", roles: ["ROLE_AUTHORITY", "ROLE_VOLUNTEER"] },
+  { id: 9, title: t('home-tasks'), description: t('home-tasks-description'), icon: "fas fa-map", link: "/tasks", roles: ["ROLE_ORGANIZATION", "ROLE_VOLUNTEER"] },
+  { id: 10, title: t('home-requests'), description: t('home-requests-description'), icon: "fas fa-map", link: "/request", roles: ["ROLE_VICTIM", "ROLE_AUTHORITY"] },
 ]);
 
 const filteredTiles = computed(() => {
@@ -68,7 +69,7 @@ onMounted(() => {
 // Watch for changes in locale and update tiles accordingly
 watch(locale, () => {
   tiles.value = [
-  { id: 1, title: t('home-raports'), description: t('home-raports-description'), icon: "fas fa-file-alt", link: "/reports", roles: ["ROLE_AUTHORITY", "ROLE_DONOR"] },
+  { id: 1, title: t('home-raports'), description: t('home-raports-description'), icon: "fas fa-file-alt", link: "/reports", roles: ["ROLE_AUTHORITY", "ROLE_DONOR", "ROLE_ORGANIZATION"] },
   { id: 2, title: t('home-profile'), description: t('home-profile-description'), icon: "fas fa-user", link: "/profile", roles: ["ROLE_USER","ROLE_VICTIM", "ROLE_VOLUNTEER", "ROLE_AUTHORITY", "ROLE_ORGANIZATION","ROLE_DONOR"] },
   { id: 3, title: t('home-resource'), description: t('home-resource-description'), icon: "fas fa-cogs", link: "/resource", roles: ["ROLE_ORGANIZATION", "ROLE_AUTHORITY", "ROLE_DONOR"] },
   { id: 4, title: t('home-map'), description: t('home-map-description'), icon: "fas fa-map", link: "/map", roles: ["ROLE_AUTHORITY","ROLE_VICTIM","ROLE_ORGANIZATION"] },
@@ -76,6 +77,8 @@ watch(locale, () => {
   { id: 6, title: t('home-volunteer'), description: t('home-volunteer-description'), icon: "fas fa-map", link: "/volunteer", roles: ["ROLE_ORGANIZATION"] },
   { id: 7, title: t('home-application'), description: t('home-application-description'), icon: "fas fa-map", link: "/application", roles: ["ROLE_ORGANIZATION"] },
   { id: 8, title: t('home-organization'), description: t('home-organization-description'), icon: "fas fa-map", link: "/organization", roles: ["ROLE_AUTHORITY", "ROLE_VOLUNTEER"] },
+  { id: 9, title: t('home-tasks'), description: t('home-tasks-description'), icon: "fas fa-map", link: "/tasks", roles: ["ROLE_ORGANIZATION", "ROLE_VOLUNTEER"] },
+  { id: 10, title: t('home-requests'), description: t('home-requests-description'), icon: "fas fa-map", link: "/request", roles: ["ROLE_VICTIM", "ROLE_AUTHORITY"] },
   ];
 });
 </script>
